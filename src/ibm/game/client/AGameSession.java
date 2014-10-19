295c74baf1b6336a5ce5ad9ada5ce4979781c11d
PeterZhan <hlzhangxt@126.com>
2014-10-19 12:56:06 -0400

    move function added

---
+++ src/ibm/game/client/AGameSession.java
@@ -0,0 +1,72 @@
+package ibm.game.client;
+import java.util.*;
+import io.netty.channel.Channel;
+
+public class AGameSession {
+    	
+	private String gameid;
+	
+	private int width;
+	private int height;
+	
+	int x;
+	int y;
+	
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public synchronized int  getWidth() {
+		return width;
+	}
+
+	public synchronized void setWidth(int width) {
+		this.width = width;
+	}
+
+	public synchronized int getHeight() {
+		return height;
+	}
+
+	public synchronized void setHeight(int height) {
+		this.height = height;
+	}
+
+	
+		
+	
+
+	public synchronized String  getGameid()  {
+		return gameid;
+	}
+
+	public AGameSession()
+	{
+	
+			
+	}
+
+
+	public synchronized void setGameid(String gameid) {
+		this.gameid = gameid;
+	}
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/ibm/game/client/PanelGame.java
@@ -0,0 +1,71 @@
+package ibm.game.client;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.util.TimerTask;
+import java.util.Timer;
+
+import javax.swing.*;
+
+public class PanelGame extends JPanel implements MouseMotionListener {
+
+	public PanelGame() {
+		super();
+		setOpaque(false); // we don't paint all our bits
+		setLayout(new BorderLayout());
+		setBorder(BorderFactory.createLineBorder(Color.black));
+		setBackground(new Color(51, 153, 255));
+		addMouseMotionListener(this);
+	}
+
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		boolean status = this.requestFocusInWindow();
+	}
+
+	@Override
+	public Dimension getPreferredSize() {
+		// Figure out what the layout manager needs and
+		// then add 100 to the largest of the dimensions
+		// in order to enforce a 'round' bullseye
+		/*
+		 * the use of "super." is very important, because otherwise the JRE will
+		 * throw a StackOverflowError. And because of the JFrame.pack() used
+		 * above, the JFrame window will be resized to adapter the Container
+		 * size.
+		 */
+		Dimension layoutSize = super.getPreferredSize();
+		int max = Math.max(layoutSize.width, layoutSize.height);
+		return new Dimension(max + 100, max + 100);
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		/*
+		 * Dimension size = getSize(); System.out.println(size.width);
+		 * System.out.println(size.height); int x = 0; int y = 0; int i = 0;
+		 * while(x < size.width && y < size.height) { g.setColor(i%2==0?
+		 * Color.red : Color.white);
+		 * g.fillOval(x,y,size.width-(2*x),size.height-(2*y)); x+=10; y+=10;
+		 * i++; }
+		 */
+		g.setColor(Color.YELLOW);
+		int x = gameClientHandler.game.getX() - 10;
+		int y = gameClientHandler.game.getY() - 10;
+
+		g.fillOval(x, y, 20, 20);
+
+	}
+
+}
